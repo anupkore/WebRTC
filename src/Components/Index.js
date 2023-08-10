5e97ce9052c766f43c3d09a745db34d49dbf70f9
unknown <anup.kore88@gmail.com>
2023-08-10 09:46:23 +0530

    Commenting

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,20 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { useRef, useState } from 'react';
+import { useRef, useState , useEffect } from 'react';
 import * as SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
 function Index()
 {
-    // const url = "http://localhost:8080"
-
-  
+    
     const localVideo = useRef();
     const remoteVideo = useRef();
     const localIdInp = useRef();
     const remoteIdInp = useRef();
-    const [audioMuted, setAudioMuted] = useState(false);
-    const [videoMuted, setVideoMuted] = useState(false);
-    let localStream;
+    const [videoEnabled, setVideoEnabled] = useState(true);
+    const [audioEnabled, setAudioEnabled] = useState(true);
+
+    const [localStream, setLocalStream] = useState(null);
     let remoteStream;
     var localPeer;
     let remoteID;
@@ -22,7 +21,9 @@ function Index()
     let stompClient;
     let subscriptions = [];
     
-    
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////  
     // ICE Server Configurations
     const iceServers = {
         iceServer: {
@@ -31,29 +32,56 @@ function Index()
     }
     
     localPeer = new RTCPeerConnection(iceServers)
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
+    // if(videoEnabled)
+    // {
+    //     navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    //     .then(stream => {
+    //         localStream = stream    
+    //         localVideo.current.srcObject = stream;
+    //         // access granted, stream is the webcam stream
+    //     })
+    //     .catch(error => {
+    //         // access denied or error occurred
+    //         console.log(error)
+    //     });
+    // }
+
+
+    // Permissions for accessing the video stream and audio stream
+
+    useEffect(() => {
+        if (videoEnabled || audioEnabled) {
+            navigator.mediaDevices.getUserMedia({ video: videoEnabled, audio: audioEnabled })  //Used for video and audio permissions
+                .then(stream => {
+                    setLocalStream(stream);
+                    localVideo.current.srcObject = stream; //setting local video stream on local video div
+                })
+                .catch(error => {
+                    console.error("Error accessing media:", error);
+                });
+        } else {
+            if (localStream) {
+                localStream.getTracks().forEach(track => track.stop());
+                setLocalStream(null);
+                localVideo.current.srcObject = null;
+            }
+        }
+    }, [videoEnabled, audioEnabled]);
     
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-        .then(stream => {
-            localStream = stream
-    
-            // console.log(stream.getTracks()[0])
-            // console.log(stream.getTracks()[1])
-            // console.log(localStream.getTracks()[0])
-            // console.log(localStream.getTracks()[1])
-    
-            localVideo.current.srcObject = stream;
-            // access granted, stream is the webcam stream
-        })
-        .catch(error => {
-            // access denied or error occurred
-            console.log(error)
-        });
-    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     function handleConnect()
     {
         // Connect to Websocket Server
-        var socket = new SockJS('https://web-rtc-react-techbrutal1151-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com:8443/websocket');
+        var socket = new SockJS('http://localhost:8080/websocket');
         stompClient = Stomp.over(socket);
         localID = localIdInp.current.value;
         console.log("My ID: " + localID)
@@ -69,19 +97,23 @@ function Index()
             subscriptions.push(testServerSubscription);
             
             
-            stompClient.subscribe('/user/' + localIdInp.current.value + "/topic/call", (call) => {
+            stompClient.subscribe('/user/' + localIdInp.current.value + "/topic/call", (call) => 
+            {
                 console.log("Step - 2");
                 console.log("Call From: " + call.body)
                 console.log("Step - 3");
                 remoteID = call.body;
                 console.log("Remote ID: " + call.body)
     
+
+                //Setting remote video stream to remote video div
                 localPeer.ontrack = (event) => {
-                    // Setting Remote stream in remote video element
-                    remoteVideo.current.srcObject = event.streams[0]
+                    console.log(event.streams[0]);
+                    remoteVideo.current.srcObject = event.streams[0];
                 }
     
     
+
                 localPeer.onicecandidate = (event) => {
                     console.log("Step - 4");
                     if (event.candidate) {
@@ -100,12 +132,18 @@ function Index()
                     }
                 }
     
+
+
+
                 // Adding Audio and Video Local Peer
                 localStream.getTracks().forEach(track => {
                     console.log("Step - 5");
                     localPeer.addTrack(track, localStream);
                 });
     
+
+
+
                 localPeer.createOffer().then(description => {
                     console.log("Step - 6");
                     localPeer.setLocalDescription(description);
@@ -117,7 +155,11 @@ function Index()
                     }))
                 })
             });
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
             stompClient.subscribe('/user/' + localIdInp.current.value + "/topic/offer", (offer) => {
                 console.log("Step - 7");
                 console.log("Offer came")
@@ -127,7 +169,14 @@ function Index()
                 console.log(typeof (new RTCSessionDescription(o)))
     
                 localPeer.ontrack = (event) => {
-                    remoteVideo.current.srcObject = event.streams[0]
+                    console.log("Step - 8");
+                    console.log(event.streams[0])
+                    console.log(event.track.kind)
+                    //if (event.track.kind === 'video') {
+                        // Set the remote stream only when a video track is received
+                        remoteVideo.current.srcObject = event.streams[0];
+                    //}
+                    
                 }
                 localPeer.onicecandidate = (event) => {
                     if (event.candidate) {
@@ -165,7 +214,12 @@ function Index()
     
                 })
             });
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
             stompClient.subscribe('/user/' + localIdInp.current.value + "/topic/answer", (answer) => {
                 console.log("Answer Came")
                 var o = JSON.parse(answer.body)["answer"]
@@ -187,13 +241,16 @@ function Index()
     
             });
     
-            
-    
             console.log("Step - 3");
             stompClient.send("/app/addUser", {}, localIdInp.current.value)
     
         })
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     
     function handleCall()
     {
@@ -201,10 +258,20 @@ function Index()
         stompClient.send("/app/call", {}, JSON.stringify({"callTo": remoteIdInp.current.value, "callFrom": localIdInp.current.value}))
     }
 
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     function handleTest()
     {
         stompClient.send("/app/testServer", {}, "Test Server")
     }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     function handleLeave()
     {
@@ -219,24 +286,86 @@ function Index()
           window.location.href="/test";
     }
 
-
-    // Inside your component function
-function toggleAudio() {
-    localStream.getAudioTracks().forEach(track => {
-        track.enabled = !audioMuted;
-    });
-    setAudioMuted(!audioMuted);
-}
-
-function toggleVideo() {
-    localStream.getVideoTracks().forEach(track => {
-        track.enabled = !videoMuted;
-    });
-    setVideoMuted(!videoMuted);
-}
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+    // const toggleVideo = async () => {
+    //     const newVideoEnabled = !videoEnabled;
+    //     try 
+    //     {
+    //         //if(newVideoEnabled===false)
+    //         //{
+    //             localStream.getVideoTracks().forEach(track => {
+    //                 track.enabled = newVideoEnabled; // Stop the current video track
+    //             });
+    //             setVideoEnabled(newVideoEnabled);
+    //         //}
+    //         // else
+    //         // {
+    //         //     const newStream = new MediaStream();
+                
+    //         //     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    //         //         stream.getVideoTracks().forEach(track => {
+    //         //             newStream.addTrack(track);
+    //         //         });
+                
+    //         //     localVideo.current.srcObject = newStream; // Update video element
+    //         //     setVideoEnabled(newVideoEnabled); // Update state
+    //         // } 
+    //     }
+    //     catch (error) 
+    //     {
+    //         console.error("Error toggling video:", error);
+    //     }
+    // };
+
+    // const toggleVideo = () => {
+    //     setVideoEnabled(!videoEnabled);
+    // };
+
+    const toggleVideo = async () => {
+        // const newVideoEnabled = !videoEnabled;
     
+        // localStream.getVideoTracks().forEach(track => {
+        //     track.enabled = newVideoEnabled; // Enable or disable the video track
+        // });
+    
+
+        // // Update localVideo srcObject to reflect the change
+        // localVideo.current.srcObject = localStream;
+        // // Update localPeer's tracks if it's active
+        // if (localPeer.connectionState === 'connected') {
+        //     localPeer.getSenders().forEach(sender => {
+        //         if (sender.track && sender.track.kind === 'video') {
+        //             sender.replaceTrack(localStream.getVideoTracks()[0]);
+        //         }
+        //     });
+            
+        // }
+
+        
+        // console.log("Called")
+        // setVideoEnabled(newVideoEnabled); // Update state
+
+        let videoTrack = localStream.getTracks().find(track => track.kind ==='video');
+        if (videoTrack.enabled)
+        {
+            videoTrack.enabled = false;
+        }
+        else
+        {
+            videoTrack.enabled = true;
+        }
+    };
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+
 function hideVideos() {
     localVideo.current.srcObject = null;
     remoteVideo.current.srcObject = null;
@@ -256,7 +385,9 @@ function hideVideos() {
 }
     
     
-    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     
     return(
@@ -286,13 +417,14 @@ function hideVideos() {
                 <div className='d-flex justify-content-center mt-5'>
                     <button id="test" className='btn btn-danger m-3' onClick={handleLeave}>End Call</button>
                     <button id="testConnection" className='btn btn-primary m-3' onClick={handleTest}>Test Connection</button>
-                    <button className={`btn ${audioMuted ? 'btn-secondary' : 'btn-primary'} m-3`} onClick={toggleAudio}>
-                        {audioMuted ? 'Unmute Audio' : 'Mute Audio'}
-                    </button>
-                    <button className={`btn ${videoMuted ? 'btn-secondary' : 'btn-primary'} m-3`} onClick={toggleVideo}>
-                        {videoMuted ? 'Turn On Video' : 'Turn Off Video'}
+                </div>
+                <div className='d-flex justify-content-center mt-5'>
+                    <button className={`btn ${videoEnabled ? 'btn-primary' : 'btn-secondary'} m-3`} onClick={toggleVideo}>
+                        {videoEnabled ? 'Turn Off Video' : 'Turn On Video'}
                     </button>
                 </div>
+
+
                 
             </div>
         </>
